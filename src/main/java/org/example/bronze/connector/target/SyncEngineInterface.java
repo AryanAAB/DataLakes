3988c805ccab958049026403d24dd563c892c482
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2026-03-19 23:46:15 +0530

    Update process method signature in SyncEngineInterface

--- src/main/java/org/example/bronze/connector/target/SyncEngineInterface.java
+++ src/main/java/org/example/bronze/connector/target/SyncEngineInterface.java
@@ -3,5 +3,5 @@ package org.example.bronze.connector.target;
 public interface SyncEngineInterface
 {
     void public sync() throws Exception;
-    void private process() throws Exception;
+    void private process(FileMetadata meta) throws Exception;
 }
