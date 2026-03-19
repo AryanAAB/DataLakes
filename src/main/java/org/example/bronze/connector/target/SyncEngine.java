70ebaf4b036b4ab69a7cbfea0fadccd80078042a
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2026-03-19 23:41:56 +0530

    Implement SyncEngineInterface in SyncEngine class

--- src/main/java/org/example/bronze/connector/target/SyncEngine.java
+++ src/main/java/org/example/bronze/connector/target/SyncEngine.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public class SyncEngine
+public class SyncEngine implements SyncEngineInterface
 {
     private static final int SNAPSHOT_INTERVAL = 2;
 
