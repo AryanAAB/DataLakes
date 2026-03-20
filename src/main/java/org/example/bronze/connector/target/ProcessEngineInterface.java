ab403b031d07504ddbf485a64f1b5bbc7ccb6562
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2026-03-20 15:51:05 +0530

    Add newFilePath parameter to process method

--- src/main/java/org/example/bronze/connector/target/ProcessEngineInterface.java
+++ src/main/java/org/example/bronze/connector/target/ProcessEngineInterface.java
@@ -4,6 +4,6 @@ import org.example.bronze.metadata.FileMetadata;
 
 public interface ProcessEngineInterface
 {
-    public void process(FileMetadata meta, VersionStore store) throws Exception;
+    public void process(FileMetadata meta, VersionStore store, Path newFilePath) throws Exception;
 }
 
