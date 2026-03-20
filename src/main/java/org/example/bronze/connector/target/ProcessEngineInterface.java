f128be4aedb8ed87ce3679fd16b21e409f941ad8
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2026-03-20 16:01:46 +0530

    Add VersionStore parameter to process method

--- src/main/java/org/example/bronze/connector/target/ProcessEngineInterface.java
+++ src/main/java/org/example/bronze/connector/target/ProcessEngineInterface.java
@@ -1,6 +1,9 @@
 package org.example.bronze.connector.target;
 
 import org.example.bronze.metadata.FileMetadata;
+import org.example.bronze.metadata.VersionStore;
+
+import java.nio.file.Path;
 
 public interface ProcessEngineInterface
 {
